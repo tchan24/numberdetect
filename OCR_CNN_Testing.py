471606b55b2fedaf0a762f1df20eb19d8878beee
tchan24 <tchan0524@gmail.com>
2024-05-19 23:11:41 -0500

    IT ACTUALLY WORKS

--- OCR_CNN_Testing.py
+++ OCR_CNN_Testing.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
-import pickle
+#import pickle
+import tensorflow as tf
 
 
 #############################
@@ -12,8 +13,10 @@ cap = cv2.VideoCapture(1)
 cap.set(3, width)
 cap.set(4, height)
 
-pickle_in = open("model_trained.p", "rb")
-model = pickle.load(pickle_in)
+#load model
+#pickle_in = open("model_trained.p", "rb")
+#model = pickle.load(pickle_in)
+model = tf.keras.models.load_model("model_trained.keras")
 
 def preProcess(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -36,7 +39,17 @@ while True:
     classindex = np.argmax(predictions)
     print(classindex)
 
-    cv2.imshow("Processed image", img)
+    # Convert the image back to a displayable format
+    display_img = img.reshape(32, 32)  # Reshape for display
+    display_img = (display_img * 255).astype(np.uint8)  # Convert back to uint8 type
+
+    display_img = cv2.resize(display_img, (320, 320), interpolation=cv2.INTER_NEAREST)
+
+    cv2.imshow("Processed image", display_img)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+#release webcam and destroy all opencv
+cap.release()
+cv2.destroyAllWindows()
